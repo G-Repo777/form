dd291062b0e46c08093c478bc982fdc34e080c16
G-Repo777 <gitrepo777@gmail.com>
2024-06-19 14:28:36 -0500

    Add files via upload

--- Create.php
+++ Create.php
@@ -16,31 +16,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Facebook_Game_Data</title>
-</head>
-<body>
-    <div class="bg-darkd-flex justify-content-center aglin-items-center w-100 vh-100">
-        <div class="bg-white w-25 rounded">
-<form method="post"> 
-        <label for="Date">Date:</label>
-        <input type="date" id="date" name="Date"><br>
-
-        <label for="Game_Name">Game Name:</label>
-        <input type="varchar" id="game_name" name="Game_Name" maxlength="20"><br>
-
-        <label for="Page_Name">Page Name:</label>
-        <input type="text" id="page_Name" name="Page_Name" maxlength="20"><br>
-
-        <label for="Page_Likes">Page Likes:</label>
-        <input type="number" id="Page_Likes" name="Page_Likes"><br>
-
-        <button class="btn btn-success" type="submit">Create</button>
-</body>
-</html>
